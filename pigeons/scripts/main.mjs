dc8768b949efb6d71a7e426f69e1e8a755af0908
Notokr <notokryt@gmail.com>
2026-02-05 09:46:37 +0100

    Made it bento!

--- pigeons/scripts/main.mjs
+++ pigeons/scripts/main.mjs
@@ -1,4 +1,6 @@
 import pigeons from "../pigeons.json" with { type: "json" };
+import BentoGrid from "https://cdn.jsdelivr.net/npm/@bentogrid/core@1.1.1/BentoGrid.min.js";
+
 
 const baseURL = "https://notokr.github.io/pigeons"
 
@@ -6,7 +8,35 @@ var bentoContainer = document.getElementById("pigeons-bento");
 
 for (const pigeon of pigeons.pigeons) {
     console.log(pigeon.name)
+    // Creating the tag
+    let pigeonContainerTag = document.createElement("div");
     let pigeonImageTag = document.createElement("img");
-    pigeonImageTag.src = `${baseURL}/images/svg/${pigeon.name}.svg`
-    bentoContainer.appendChild(pigeonImageTag)
+
+    pigeonContainerTag.classList.add("pigeon-container");
+    pigeonImageTag.classList.add("pigeon-tile");
+
+    if (pigeon.types.includes("s")) {
+        pigeonImageTag.src = `${baseURL}/images/svg/${pigeon.name}.svg`
+    } else if (pigeon.types.includes("p")) {
+        pigeonImageTag.src = `${baseURL}/images/png/${pigeon.name}.png`
+    } else if (pigeon.types.includes("c")) {
+        pigeonImageTag.src = `${baseURL}/images/croquis/${pigeon.name}.png`
+    }
+    if (Math.random() < 0.2) {
+        pigeonContainerTag.setAttribute("data-bento", "2x2")
+    } else {
+        pigeonContainerTag.setAttribute("data-bento", "1x1")
+    }
+    pigeonImageTag.style.width = "100%";
+
+    pigeonContainerTag.appendChild(pigeonImageTag);
+    bentoContainer.appendChild(pigeonContainerTag);
 }
+
+const myBento = new BentoGrid({
+    balanceFillers: true,
+    columns: 4,
+    cellGap: 6,
+    target: "#pigeons-bento",
+    aspectRatio: 1,
+});
